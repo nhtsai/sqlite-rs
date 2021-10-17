a314bfad36445569e8c6a36dfddeaefe1c60b2ec
Nathan Tsai <natesigh@gmail.com>
2021-10-17 00:10:42 -0700

    add simple repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io::{self, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd: String = String::new(); // consider String::with_capacity()
+    loop {
+        print_prompt();
+        
+        // Read in commandSt
+        io::stdin().read_line(&mut cmd).expect("Failed to read command.");
+        
+        
+        cmd = cmd.trim().to_string();
+        
+        match cmd.as_str() {
+            ".exit" => {break;}
+            _ => {
+                println!("Unrecognized command '{}'.", cmd);
+                cmd = String::new();
+            }
+        }
+    }
+}
+
+fn print_prompt() {
+    print!("db > ");
+    io::stdout().flush().unwrap();
 }
